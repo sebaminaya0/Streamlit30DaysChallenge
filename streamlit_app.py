810653737f95dc2e953264c68cda240ea112ded3
sebaminaya0_ <sebastian.minaya1998@gmail.com>
2023-08-19 12:12:09 -0500

    last pandas profiling adjustment

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import numpy as np
 import altair as alt
 import pandas as pd
 import streamlit as st
-import pandas_profiling
+# import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 import datetime as dt
 import time
